a7548d15cfa97bfa3592204efbb6470e36f0f47a
floatingbug <sieber.thomas@yahoo.com>
2023-04-14 12:31:22 +0200

    modified login

--- vue-quiz/src/add.test.js
+++
@@ -1,9 +0,0 @@
-let add = require('./add');
-
-describe('add', ()=>{
-  test('should return the sum of two numbers', ()=>{
-    const result = add(2, 5);
-
-    expect(result).toBe(7)
-  })
-})
--- vue-quiz/src/components/login_component.vue
+++ vue-quiz/src/components/login_component.vue
@@ -25,23 +25,55 @@
 	justify-content: center;
 	align-items: center;
 	height: 80vh; 
+	background-color: grey;
 }
 
 form {
 	display: flex;
 	flex-direction: column;
+	justify-content: space-around;
 	border: solid .2rem LawnGreen;
 	height: 50%;
-	width: 50%;
+	width:60%;
+	font-size: 1.5rem;
 }
 
 form>div {
 	display: flex;
-	flex-direction: row;
+	flex-wrap: wrap;
 }
 
 form>div * {
 	flex:1;
 }
 
+form>div>label {
+	flex-basis: 100px;
+	margin-left: 10%;
+}
+
+form>div>input {
+	flex: 8;
+	max-width: 100%;
+	min-width: 45%;
+	margin-left: 10%;
+	margin-right: 10%;
+}
+
+form>div>input[type="submit"] {
+	color: red; 
+}
+
+@media(max-width: 720px){
+	form{
+		min-width: 100%;
+	}
+}
+
+@media(min-width: 1040px){
+	form{
+		width: 42%;
+		height: 60%;
+	}
+}
 </style>
